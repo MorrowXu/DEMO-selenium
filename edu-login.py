2392a2e3ffd8b4e40ade6fce1d4354fb0f016682
Morrow <464580843@qq.com>
2018-01-08 11:04:18 +0800

    2018.1.8 commit

--- edu-login.py
+++ edu-login.py
@@ -5,12 +5,12 @@ from selenium import webdriver
 from PIL import Image,ImageEnhance
 import os
 import time
-import urllib
+# import urllib
 import pytesseract
 if 'HTTP_PROXY' in os.environ: del os.environ['HTTP_PROXY'] # 取消http代理
-login_dict = {'url':'http://****/***',
-			  'username':'*****',
-			  'password':'*****'}
+login_dict = {'url':'http://xueli.xjtudlc.com/login1.aspx',
+			  'username':'117093943010052',
+			  'password':'xuhaoran123ufo'}
 
 
 def code_ocr(pic_filename):
@@ -24,7 +24,7 @@ def code_ocr(pic_filename):
 	v_code = pytesseract.image_to_string(img_ehc)
 	verifycode = ''
 	for i in v_code:
-		# 对tesseract.exe引擎识别出来的字符串进行一个判断,如果不为字母或数字则不进入统计
+		# 对tesseract.exe引擎识别出来的字符串进行判断,如果不为字母或数字则不进入统计
 		if i.isalpha() or i.isdigit():
 			verifycode += i
 	return verifycode
@@ -37,43 +37,53 @@ def autologin():
 	dr = webdriver.Firefox()
 	# dr.set_window_size(400,450)
 	dr.get(login_dict['url'])
-	# time.sleep(1)
-	# dr.refresh()
-	time.sleep(2)
+	error_num = 1
+	while True:
+		time.sleep(3)
+		txtUserName = dr.find_element_by_id('txtUserName')
+		txtUserName.clear()
+		txtUserName.send_keys(login_dict['username'])
+		time.sleep(1)
+		txtPassword = dr.find_element_by_id('txtPassword')
+		txtPassword.clear()
+		txtPassword.send_keys(login_dict['password'])
+		# dr.find_element_by_xpath('//*[@id="form1"]/ul/li[3]/a').click() # 刷新一次验证码确保识别的验证码没过期
+		time.sleep(1)	
 
-	txtUserName = dr.find_element_by_id('txtUserName')
-	txtUserName.send_keys(login_dict['username'])
-	time.sleep(1)
-	txtPassword = dr.find_element_by_id('txtPassword')
-	txtPassword.send_keys(login_dict['password'])
-	# dr.find_element_by_xpath('//*[@id="form1"]/ul/li[3]/a').click() # 刷新一次验证码确保识别的验证码没过期
-	time.sleep(1)
+		# ...........................验证码识别...........................
+		# pic_code = dr.find_element_by_xpath('//*[@id="VerifyCode"]')
+		# pic_link = pic_code.get_attribute('src')
+		# response = urllib.urlretrieve(pic_link,'yzm.jpg') # 网页上抓取验证码图片
+		pic_filename = 'C:\\Users\\Administrator\\Desktop\\screen_shot.png'
+		dr.get_screenshot_as_file(pic_filename) # 截浏览器整图
+		img1 = Image.open(pic_filename)
+		box = (780,236,864,256)  # box(left, upper, right, lower)
+		verifycode_pic = img1.crop(box)
+		verifycode_pic.save('verifycode.png')
 
-	# ...........................验证码识别...........................
-	# pic_code = dr.find_element_by_xpath('//*[@id="VerifyCode"]')
-	# pic_link = pic_code.get_attribute('src')
-	# print pic_link
-	pic_filename = 'C:\\Users\\Administrator\\Desktop\\verifycode.png'
-	# response = urllib.urlretrieve(pic_link,pic_filename) # 网页上抓取验证码图片
-	dr.get_screenshot_as_file(pic_filename) # 截浏览器整图
-	img1 = Image.open(pic_filename)
-	box = (780,236,864,256)  # box(left, upper, right, lower)
-	verifycode_pic = img1.crop(box)
-	verifycode_pic.save('yzm.png')
+		the_verifycode = code_ocr('verifycode.png')
+		txtVerifyCode = dr.find_element_by_id('txtVerifyCode')
+		txtVerifyCode.clear()
+		txtVerifyCode.send_keys(the_verifycode)
+		time.sleep(1)
+		# ----------------------------------------------------------------
 
-	the_verifycode = code_ocr('yzm.png')
-	print the_verifycode
-	txtVerifyCode = dr.find_element_by_id('txtVerifyCode')
-	txtVerifyCode.send_keys(the_verifycode)
-	time.sleep(1)
-	# ----------------------------------------------------------------
+		btnlogin = dr.find_element_by_id('lbtnLogin')
+		btnlogin.click()
+		try:
+			dr.find_element_by_xpath('//*[@id="mainFrame"]') # 如果登陆成功会打破循环进入下一步
+			print '尝试%d次登录成功' % error_num
+			break
+		except Exception as e:
+			print '失败%d次,错误代码:' % error_num, e
+			error_num += 1
+			_alert = dr.switch_to_alert()
+			_alert.accept() # 如果登陆失败会循环再来一次
 
-	btnlogin = dr.find_element_by_id('lbtnLogin')
-	btnlogin.click()	
 	input()
 	w1 = dr.find_element_by_xpath('//*[@id="mainFrame"]') 
-	# 由于网页结构为frameset/frame,所以需要定位到该frame下,并swith_to.      frame需要切/frameset不需要切
-	dr.swith_to.frame(w1) # 切换到该frameset下	
+	# 由于网页结构为frameset/frame,所以需要定位到该frame下,并switch_to.      frame需要切/frameset不需要切
+	dr.switch_to.frame(w1) # 切换到该frameset下	
 
 	ic = dr.find_element_by_xpath('//*[@id="InCourse"]') # 重新对页面元素进行定位
 	ic.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_\
@@ -85,9 +95,8 @@ def autologin():
 	# next: 解决窗口焦点句柄切换问题
 	all_handles = dr.window_handles # 获得所有窗口句柄
 	dr.switch_to_window(all_handles[-1]) # 切换窗口焦点句柄到最后一个页面	
-
+	input()
 	# next: 解决弹出警报处理 ;未完
 
 if __name__ == '__main__':
 	autologin()
-
